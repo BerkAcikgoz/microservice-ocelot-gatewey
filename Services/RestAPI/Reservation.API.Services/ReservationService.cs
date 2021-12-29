07a40bbc6018cb70132c07f70c406ae2ef432e3e
Berk Açıkgöz <berk_acikgoz@kocsistem.com.tr>
2021-12-29 21:18:33 +0300

    Add project files.

---
+++ Services/RestAPI/Reservation.API.Services/ReservationService.cs
@@ -0,0 +1,22 @@
+﻿using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+using System;
+
+namespace Reservation.API.Services
+{
+    public class ReservationService : IReservationService
+    {
+        public ReservationDTO GetReservationByBkgNumber(int BkgNumber)
+        {
+            return new ReservationDTO
+            {
+                Id = (new Random()).Next(199),
+                Amount = 1000,
+                BkgDate = DateTime.Now,
+                BkgNumber = BkgNumber,
+                CheckinDate = DateTime.Now.AddDays(30),
+                CheckoutDate = DateTime.Now.AddDays(37)
+            };
+        }
+    }
+}
