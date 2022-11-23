dfef44276da6ded2da71d5c77cb11535d3ffd694
Berk Açıkgöz <berk_acikgoz@kocsistem.com.tr>
2022-11-23 10:34:38 +0300

    add project

---
+++ Services/ContactAPI/Contact.API/Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using Contact.API.Infrastructure;
+using Contact.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contact.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+        public ContactController(IContactService contactService)
+        {
+            this._contactService = contactService;
+        }
+
+        [HttpGet("{id}")]
+        public ContactDTO Contact(int id)
+        {
+            return this._contactService.GetContactById(id);
+        }
+
+    }
+}
